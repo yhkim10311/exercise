16c41b42f7c091425bbe00c6cf3169d82428852f
김영훈 <yhkim1031@woowahan.com>
2021-08-09 20:10:08 +0900

    third branch first commit

--- src/main/java/com/ex/Excercise.java
+++ src/main/java/com/ex/Excercise.java
@@ -1,85 +1,12 @@
 package com.ex;
 
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.Set;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Excercise {
     public static void main(String[] args) {
-        int[] arr ;
-        int index = 0;
-        for(arr= new int[5];;){
-            if(index==10){
-
-            }else{
-                break;
-            }
-        }
-
-        // 클래스 이름을 Class 객체로 변환
-        Class<? extends Set<String>> cl = null;
-        try {
-            cl = (Class<? extends Set<String>>) // 비검사 형변환!
-                    Class.forName(args[0]);
-        } catch (ClassNotFoundException e) {
-            fatalError("클래스를 찾을 수 없습니다.11");
-        }
-
-        // 생성자를 얻는다.
-        Constructor<? extends Set<String>> cons = null;
-        try {
-            cons = cl.getDeclaredConstructor();
-        } catch (NoSuchMethodException e) {
-            fatalError("매개변수 없는 생성자를 찾을 수 없습니다.11");
-        }
-
-
-        // 집합의 인스턴스를 만든다.
-        Set<String> s = null;
-        try {
-            s = cons.newInstance();
-        } catch (IllegalAccessException e) {
-            fatalError("생성자에 접근할 수 없습니다.11");
-        } catch (InstantiationException e) {
-            fatalError("클래스를 인스턴스화할 수 없습니다.1211");
-        } catch (InvocationTargetException e) {
-            fatalError("생성자가 예외를 던졌습니다: 1" + e.getCause());
-        } catch (ClassCastException e) {
-            fatalError("Set을 구현하지 않은 클래스입니다.1");
-        }
-
-        // 생성한 집합을 사용한다.
-        s.addAll(Arrays.asList(args).subList(1, args.length));
-        System.out.println(s);
-    }
-
-    private static void fatalError(String msg) {
-        System.err.println(msg);
-        System.exit(1);
-    }
-
-}
-
-class Thread_1 extends Thread {
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        for(int i=0; i<300; i++) System.out.print("-");
-
-        System.out.print("<<th1 end>>>>>>>");
-    }
-}
-
-class Thread_2 extends Thread {
-    @Override
-    public void run() {
-        for(int i=0; i<300; i++) System.out.print("|");
-        System.out.print("<<th2 end>>1");
+        SpringApplication.run(Excercise.class);
     }
 }
